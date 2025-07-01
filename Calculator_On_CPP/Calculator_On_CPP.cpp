0b8204a72b3d06a5558ea1e81e6d8c31879e762b
NikoVisioN <darkmer3y@gmail.com>
2025-07-01 22:06:09 +0200

    Add project files.

---
+++ Calculator_On_CPP/Calculator_On_CPP.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double a, b, result;
+	char arifmetic;
+
+	cout << fixed << setprecision(5); 
+
+	while (1)
+	{
+		cout << "Enter first number:" << endl;
+
+
+		a = 0;
+		b = 0;
+		result = 0;
+
+		cin >> a >> arifmetic >> b;
+
+		switch (arifmetic)
+		{
+		case '+':
+			result = a + b;
+			cout << "Result: " << result << endl;
+			break;
+		case '-':
+			result = a - b;
+			cout << "Result: " << result << endl;
+			break;
+		case '*':
+			result = a * b;
+			cout << "Result: " << result << endl;
+			break;
+		case '/':
+			if (b != 0)
+			{
+				result = a / b;
+				cout << "Result: " << result << endl;
+			}
+			else
+			{
+				cout << "Error: Division by zero!" << endl;
+			}
+			break;
+		
+		default:
+		cout << "Error: Invalid operator!" << endl;
+		continue; // Skip to the next iteration if the operator is invalid
+		}
+
+		cout << endl << a << arifmetic << b << " = " << result << endl << " -------------------------------- " << endl;
+	}
+	return 0;
+}
